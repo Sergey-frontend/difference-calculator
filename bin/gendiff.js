6b5466cf5a596d0a1d4efd3a2aa851bdbd2fcc6f
Sergey T <tolstikovsn@gmail.com>
2023-01-05 00:28:41 +0500

    stage 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,10 @@ const { program } = require('commander');
 
 program
   .name('gendiff')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
 
 program.parse()
