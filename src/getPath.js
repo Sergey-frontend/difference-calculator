a45ef5579ef11cf87ed802c9cda5d1ad66f3fc76
Sergey T <tolstikovsn@gmail.com>
2023-01-09 21:11:43 +0500

    update getPath

--- src/getPath.js
+++ src/getPath.js
@@ -1,6 +1,10 @@
-import path from 'node:path';
+import path, { parse } from 'node:path';
 import process from 'node:process';
+// import fs from 'fs
+
 
 const getFullPath = (filename) => path.resolve(process.cwd(), filename);
+// const extractFormat = (filepath) => path.extname(filepath).slice(1);
+// const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath))
 
 export default getFullPath;
