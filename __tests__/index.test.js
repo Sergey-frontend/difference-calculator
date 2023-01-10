e427439693a02b5392f55173f5868b32e5bbb568
Sergey T <tolstikovsn@gmail.com>
2023-01-11 00:24:54 +0500

    add test to compare two .json file

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import gendiff from "../src/index.js";
+import getFullPath from "../src/getPath.js";
+
+let result2json;
+
+beforeAll(() => {
+    result2json = fs.readFileSync(getFullPath('result2json.txt'), 'utf8')
+});
+
+test('compare two json files', () => {
+    expect(gendiff('file1.json', 'file2.json')).toEqual(result2json);
+});
