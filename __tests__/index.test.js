1788a82e8c1d5646e46aa55517169cf6a85aba03
Sergey T <tolstikovsn@gmail.com>
2023-01-12 19:09:15 +0500

    add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,22 @@
-import { expect, test } from '@jest/globals';
+import { beforeAll, expect, test } from '@jest/globals';
 import fs from 'fs';
 import gendiff from '../src/index.js';
 import getFullPath from '../src/getPath.js';
 
-const result2json = fs.readFileSync(getFullPath('result2json.txt'), 'utf8');
+let result;
+
+beforeAll(() => {
+  result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
+});
 
 test('compare two json files', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(result2json);
+  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
+});
+
+test('compare two yml/yaml files', () => {
+  expect(gendiff('file1.yml', 'file2.yaml')).toEqual(result);
+});
+
+test('compare json and yml/yaml files', () => {
+  expect(gendiff('file1.json', 'file2.yaml')).toEqual(result);
 });
