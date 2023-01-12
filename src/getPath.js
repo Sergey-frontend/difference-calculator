a432f00d7e2f915582d7c7bcfd17e5958eeb2669
Sergey T <tolstikovsn@gmail.com>
2023-01-12 19:05:08 +0500

    add new functionality

--- src/getPath.js
+++ src/getPath.js
@@ -10,7 +10,5 @@ const getFullPath = (filename) => path.join(__dirname, '..', '__fixtures__', fil
 
 /* первая версия, getFullPath  не запоминает путь до папки */
 // const getFullPath = (filename) => path.resolve(process.cwd(), filename);
-// const extractFormat = (filepath) => path.extname(filepath).slice(1);
-// const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath))
 
 export default getFullPath;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 // основной файл для итоговой функции поиска разницы
-import fs from 'fs';
 import build from './build.js';
 import render from './render.js';
-import getFullPath from './getPath.js';
+import parse from './parsers.js';
 
 const gendiff = (filepath1, filepath2) => {
   const paths = [filepath1, filepath2];
-  const data = paths.map((filepath) => {
-    const fullPath = getFullPath(filepath);
-    const content = fs.readFileSync(fullPath, 'utf-8');
-    return JSON.parse(content);
-  });
+  const data = paths.map((filepath) => parse(filepath));
   const tree = build(data);
   return render(tree);
 };
