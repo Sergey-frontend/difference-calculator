08cc1015b75115d079c57972d67381b36fad6d7b
Sergey T <tolstikovsn@gmail.com>
2023-01-17 14:17:15 +0500

    add fn to search nested obj

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,43 @@
+import _ from 'lodash';
 
-const stylish = (file1, file2) => {
-    
-}
+const symbols = { removed: '-', added: '+', unchanged: ' ' };
+
+const setSpace = (num, str = ' ') => str.repeat(num * 4 - 2);
+
+const makeString = (value, num = 1) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const keys = _.keys(value);
+  const result = keys.map((key) => {
+    const nestedKey = value[key];
+    return `${setSpace(num + 1)}  ${key}: ${makeString(nestedKey, num + 1)}`;
+  });
+  return `{\n${result.join('\n')}\n  ${setSpace(num)}}`;
+};
+
+const stylish = (build) => {
+  const iter = (node, acc = 1) => {
+    const {
+      type, key, value, meta, children,
+    } = node;
+    switch (type) {
+      case 'added':
+      case 'removed':
+      case 'unchanged':
+        return `${setSpace(acc)}${symbols[type]} ${key}: ${makeString(value, acc)}`;
+      case 'updated':
+        return `${setSpace(acc)}${symbols.removed} ${key}: ${makeString(meta.oldValue, acc)}\n${setSpace(acc)}${symbols.added} ${key}: ${makeString(value, acc)}`;
+      case 'nested': {
+        const objectResult = children.flatMap((child) => iter(child, acc + 1));
+        return `${setSpace(acc)}  ${key}: {\n${objectResult.join('\n')}\n${setSpace(acc)}  }`;
+      }
+      default: throw new Error(`Unknown type: ${type}`);
+    }
+  };
+
+  const result = build.map((item) => iter(item));
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default stylish;
