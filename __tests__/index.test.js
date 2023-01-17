5316c9daeaa1bce80ecec7dc69dcab2fc3c148ca
Sergey T <tolstikovsn@gmail.com>
2023-01-17 14:13:01 +0500

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,15 @@
 import { beforeAll, expect, test } from '@jest/globals';
-import fs from 'fs';
+import { readFileSync } from 'node:fs';
 import gendiff from '../src/index.js';
 import getFullPath from '../src/getPath.js';
+import getFormat from '../src/formatters/renderFormat.js';
 
 let result;
+let testingObj;
 
 beforeAll(() => {
-  result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
+  result = readFileSync(getFullPath('result.txt'), 'utf8');
+  testingObj = { a: 1, b: 2, type: 'unknownType' };
 });
 
 test('compare two json files', () => {
@@ -19,4 +22,9 @@ test('compare two yml/yaml files', () => {
 
 test('compare json and yml/yaml files', () => {
   expect(gendiff('file1.json', 'file2.yaml')).toEqual(result);
+});
+
+test('Unknown formats', () => {
+  expect(getFormat(testingObj, 'html')).toEqual(console.error('Unknown format html'));
+  expect(getFormat(testingObj, [])).toEqual(console.error('Unknown format []'));
 });
