3f7149f8f8dce8823220c3283c4004e6207781cd
Sergey T <tolstikovsn@gmail.com>
2023-01-25 13:53:12 +0500

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { cwd } from 'node:process';
@@ -6,37 +6,16 @@ import gendiff from '../src/index.js';
 
 const getFixturesPath = (filepath) => resolve(cwd(), '__fixtures__', filepath);
 
-const stylishResult = readFileSync(getFixturesPath('stylish-result.txt'), 'utf-8');
-const plainResult = readFileSync(getFixturesPath('plain-result.txt'), 'utf-8');
-const jsonResult = readFileSync(getFixturesPath('json-result.txt'), 'utf-8');
+describe.each([
+  ['json'], ['yaml'],
+])('$s extension', (extension) => {
+  test.each([
+    ['stylish'], ['plain'],['json']
+  ])('%s formatter', (formatter) => {
+    const file1Path = `__fixtures__/file1.${extension}`;
+    const file2Path = `__fixtures__/file2.${extension}`;
+    const expected = readFileSync(getFixturesPath(`${formatter}-result.txt`), 'utf-8');
 
-const stylishCase = [
-  ['__fixtures__/file1.json', '__fixtures__/file2.json', stylishResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', stylishResult],
-  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', stylishResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.json', stylishResult],
-];
-const plainCase = [
-  ['__fixtures__/file1.json', '__fixtures__/file2.json', plainResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', plainResult],
-  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', plainResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.json', plainResult],
-];
-const jsonCase = [
-  ['__fixtures__/file1.json', '__fixtures__/file2.json', jsonResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.yaml', jsonResult],
-  ['__fixtures__/file1.json', '__fixtures__/file2.yaml', jsonResult],
-  ['__fixtures__/file1.yml', '__fixtures__/file2.json', jsonResult],
-];
-
-test.each(stylishCase)('stylish output', (file1, file2, stylish) => {
-  expect(gendiff(file1, file2)).toEqual(stylish);
-});
-
-test.each(plainCase)('plain output', (file1, file2, plain) => {
-  expect(gendiff(file1, file2, 'plain')).toEqual(plain);
-});
-
-test.each(jsonCase)('json output', (file1, file2, json) => {
-  expect(gendiff(file1, file2, 'json')).toEqual(json);
+    expect(gendiff(file1Path, file2Path, formatter)).toBe(expected);
+  });
 });
